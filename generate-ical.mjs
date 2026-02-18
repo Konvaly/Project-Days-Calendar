cfd7a0d89ee7a0d0ae69eb43d3fdf4a57294e354
Konvaly <konvaly@gmail.com>
2026-02-18 23:46:40 +0000

    Use shared recurring day engine in ICS generator script

--- generate-ical.mjs
+++ generate-ical.mjs
@@ -1,7 +1,13 @@
 // This is a placeholder file which shows how you can access functions and data defined in other files. You can delete the contents of the file once you have understood how it works.
 // It can be run with `node`.
 
-import { getGreeting } from "./common.mjs";
+import { calculateRecurringDay } from "./common.mjs";
 import daysData from "./days.json" with { type: "json" };
 
-console.log(`{getGreeting()} - there are ${daysData.length} known days`);
+// Temporary verification: calculate commemorative days for one example year
+const exampleYear = 2024;
+
+for (const dayDef of daysData) {
+  const isoDate = calculateRecurringDay(exampleYear, dayDef);
+  console.log(`${dayDef.name}: ${isoDate}`);
+}
