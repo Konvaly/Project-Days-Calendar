4d1219c65f08ae61fdf386a2c436a93cc436aeed
Konvaly <konvaly@gmail.com>
2026-02-18 23:51:32 +0000

    Create base ICS calendar file skeleton

--- generate-ical.mjs
+++ generate-ical.mjs
@@ -1,13 +1,19 @@
 // This is a placeholder file which shows how you can access functions and data defined in other files. You can delete the contents of the file once you have understood how it works.
 // It can be run with `node`.
 
+import fs from "node:fs";
 import { calculateRecurringDay } from "./common.mjs";
 import daysData from "./days.json" with { type: "json" };
 
-// Temporary verification: calculate commemorative days for one example year
-const exampleYear = 2024;
+// Create minimal ICS calendar skeleton
+const icsContent = [
+  "BEGIN:VCALENDAR",
+  "VERSION:2.0",
+  "PRODID:-//Days Calendar Project//EN",
+  "END:VCALENDAR",
+].join("\n");
 
-for (const dayDef of daysData) {
-  const isoDate = calculateRecurringDay(exampleYear, dayDef);
-  console.log(`${dayDef.name}: ${isoDate}`);
-}
+// Write to file
+fs.writeFileSync("days.ics", icsContent);
+
+console.log("days.ics file created");
